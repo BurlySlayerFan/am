b6347a621f91bf375b085e32d02adde0ccd08569
Aaron Haskins <haskins.aaron@outlook.com>
2018-09-24 16:53:57 +0100

    Added a couple of config options to am_install.sh, removed some hard coded parts

--- am_install.sh
+++ am_install.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-export workspace="/home/user/am/"
+export workspace="$(echo $HOME)/am/am/"
 
 echo "Installing..."
-
 echo "http or https (http)?"
 read protocol
 protocol=${protocol:-http}
@@ -23,10 +22,18 @@ echo "Password (password)"
 read admin_pwd
 admin_pwd=${admin_pwd:-password}
 
+echo "Config Directory ($HOME/openam)"
+read cfg_dir
+cfg_dir=${cfg_dir:-$(echo $HOME/openam)}
+
+echo "Cookie Domain ($hostname)"
+read cookie_domain
+cookie_domain=${cookie_domain:-"$hostname"}
+
 #export host="http://openam.example.com:8080/openam"
 #export admin_pwd="password"
 
 export host=$protocol://$hostname:$port/$context
 export admin_pwd
 
-$workspace/amster_6/amster $workspace/amster_scripts/am_basic_install/am_install.amster -D HOST=${host} -D ADMIN_PWD=${admin_pwd}
+$workspace/amster_6/amster $workspace/amster_scripts/am_basic_install/am_install.amster -D HOST=${host} -D ADMIN_PWD=${admin_pwd} -D CFGDIR=${cfgDir}
