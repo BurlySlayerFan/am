b8128f57bc853e58da0fc9f7bead071776b09704
Aaron Haskins <haskins.aaron@outlook.com>
2018-09-25 16:54:28 +0100

    am_install.sh will install a default AM setup by pressing enter. Anything else allows for a custom install, like the install wizard.

--- am_install.sh
+++ am_install.sh
@@ -1,39 +1,52 @@
 #!/bin/bash
 export workspace="$(echo $HOME)/am/am"
 
-echo "Installing..."
-echo "http or https (http)?"
-read protocol
-protocol=${protocol:-http}
+# default installation variables
+port=8080
+context="openam"
+admin_pwd="password"
+cfg_dir="$(echo $HOME/openam)"
+cookie_domain=$hostname
 
-echo "Hostname? (openam.example.com)"
-read hostname
-hostname=${hostname:-openam.example.com}
+# press enter for default installation or enter anything else to start a custom installation
+echo "Install type? (default)"
+read install_type
+install_type=${install_type:-default}
 
-echo "Port (8080)"
-read port
-port=${port:-8080}
+if [ $install_type != "default" ]; then
+        echo "HTTP?"
+        read protocol
+        protocol=${protocol:-http}
 
-echo "Context (openam)"
-read context
-context=${context:-openam}
+        echo "Hostname? (openam.example.com)"
+        read hostname
+        hostname=${hostname:-openam.example.com}
 
-echo "Password (password)"
-read admin_pwd
-admin_pwd=${admin_pwd:-password}
+        echo "Port (8080)"
+        read port
+        port=${port:-8080}
 
-echo "Config Directory ($HOME/openam)"
-read cfg_dir
-cfg_dir=${cfg_dir:-$(echo $HOME/openam)}
+        echo "Context (openam)"
+        read context
+        context=${context:-openam}
 
-echo "Cookie Domain ($hostname)"
-read cookie_domain
-cookie_domain=${cookie_domain:-"$hostname"}
+        echo "Password (password)"
+        read admin_pwd
+        admin_pwd=${admin_pwd:-password}
 
-#export host="http://openam.example.com:8080/openam"
-#export admin_pwd="password"
+        echo "Config Directory ($HOME/openam)"
+        read cfg_dir
+        cfg_dir=${cfg_dir:-$(echo $HOME/openam)}
 
-export host=$protocol://$hostname:$port/$context
-export admin_pwd
+        echo "Cookie Domain ($hostname)"
+        read cookie_domain
+        cookie_domain=${cookie_domain:-"$hostname"}
+
+        export host=$protocol://$hostname:$port/$context
+        export admin_pwd
+else
+        export host=$protocol://$hostname:$port/$context
+        export admin_pwd
+fi
 
 $workspace/amster_6/amster $workspace/amster_scripts/am_basic_install/am_install.amster -D HOST=${host} -D ADMIN_PWD=${admin_pwd} -D CFG_DIR=${cfg_dir} -D COOKIE_DOMAIN=${cookie_domain}
